a7bc9e8dc2dfd9a1552344b9172f18c0c9a5ce4d
Yurii Shestakov <yshestakov@gmail.com>
2019-02-13 13:54:27 +0200

    spdk.spec: add mlnx specific files to the rpm
    * Adapt build scripts from v19.04.y to v19.04.1.y to v19.07.y
    * Added nvme_proxy service
    * Fixed spdk.spec
    * Added nvmf_proxy service and configs
    * Fixed RPC code to make it Python2 compatible
    
    Signed-off-by: Yurii Shestakov <yshestakov@gmail.com>

--- scripts/rpc/helpers.py
+++ scripts/rpc/helpers.py
@@ -7,7 +7,8 @@ def deprecated_alias(old_name):
     def wrap(f):
         def old_f(*args, **kwargs):
             ret = f(*args, **kwargs)
-            print("{} is deprecated, use {} instead.".format(old_name, f.__name__), file=sys.stderr)
+            sys.stderr.write("{} is deprecated, use {} instead.\n".format(
+                old_name, f.__name__))
             return ret
         old_f.__name__ = old_name
         deprecated_aliases[old_name] = f.__name__
