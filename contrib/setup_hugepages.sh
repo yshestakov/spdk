688eb30f8d220b1b672c748b072ecc140821792e
Yurii Shestakov <yshestakov@gmail.com>
2020-02-12 16:56:06 +0200

    nvmf_tgt: cleaned up hugepage allocation procedure
    Signed-off-by: Yurii Shestakov <yuriis@mellanox.com>

--- contrib/setup_hugepages.sh
+++ contrib/setup_hugepages.sh
@@ -1,25 +1,5 @@
 #!/bin/bash -e
-# libhugetlbfs-utils
-hugeadm --create-mounts
-fn=/sys/kernel/mm/hugepages/hugepages-524288kB/nr_hugepages
-if [ -e "$fn" ] ; then
-  n=$(cat $fn)
-  if [ "$n" -eq 0 ] ; then
-    echo 6 > $fn
-  fi
-fi
-fn=/sys/kernel/mm/hugepages/hugepages-1048576kB/nr_hugepages
-if [ -e "$fn" ] ; then
-  n=$(cat $fn)
-  if [ "$n" -eq 0 ] ; then
-    echo 3 > $fn
-  fi
-fi
-fn=/sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
-if [ -e "$fn" ] ; then
-  n=$(cat $fn)
-  if [ "$n" -lt 2048 ] ; then
-    echo 2048 > $fn
-  fi
-fi
-exit 0
+# Dependency on package:
+#   libhugetlbfs-utils @ CentOS
+#   hugepages @ Ubuntu
+exec /usr/bin/hugeadm --pool-pages-min DEFAULT:${MIN_HUGEMEM:-2G}
